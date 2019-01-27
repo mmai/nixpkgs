baf0a4e33a8712e000c1f8fb16160e11545ea443
Henri Bourcereau <henri.bourcereau@gmail.com>
2019-01-27 11:56:45 +0100

    python: requests-http-signature: init at 0.1.0

---
+++ pkgs/development/python-modules/requests-http-signature/default.nix
@@ -0,0 +1,26 @@
+{ stdenv
+, buildPythonPackage
+, fetchPypi
+, requests
+}:
+
+buildPythonPackage rec {
+  pname = "requests-http-signature";
+  version = "0.1.0";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "0e39d928469e6f1411e3bffca74a280ac9375d4fa5bf03552974f0ba4ff4c37a";
+  };
+  buildInputs = [ requests ];
+
+  # missing files for tests in pypi release (keys)
+  doCheck = false;
+
+  meta = with stdenv.lib; {
+    description = "A Requests auth module for HTTP Signature";
+    homepage = "https://github.com/kislyuk/requests-http-signature";
+    licence = licences.apache2;
+    maintainers = with maintainers; [ mmai ];
+  };
+}
